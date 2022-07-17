a03b87437f71166e011eddae659c95db1656df35
Victor Haynes <haynesvic@gmail.com>
2022-07-17 15:17:07 -0500

    commit tree c84571dee060a5fb801324d820f87cbce61985be

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Victor";
+const city = "Chicago";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
